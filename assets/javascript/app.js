cdb9b6c2600daab37c1fe20563d750634cbaa784
Neo <echiang73@gmail.com>
2019-02-26 10:20:48 -0700

    enable gif animation to display, still need to finalize css and mobile query

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,104 +2,128 @@
 var triviaQuestions = [{
 	question: "Who does Bruce Banner become?",
 	answerList: ["Spider-Man", "Iron Man", "Hulk", "Captain America"],
-	answer: 2
-	// answerjpg: "url: ../images/hulk.jpg"; // is this correct?
+	answer: 2,
+	answerGif: "hulk.gif",
 }, {
 	question: "Who is Peter Parker?",
 	answerList: ["Captain America", "Hulk", "Spider-Man", "Ironman"],
-	answer: 2
+	answer: 2,
+	answerGif: "spiderman.gif",
 }, {
 	question: "Where is Wakanda located?",
 	answerList: ["South America", "Africa", "Antarctica", "Australia"],
-	answer: 1
+	answer: 1,
+	answerGif: "wakandaforever.gif",
 }, {
 	question: "In 'Captain America: Civil War', who rules Wakanda?",
 	answerList: ["HYDRA", "Steve Rogers", "Black Panther", "Winter Soldier"],
-	answer: 2
+	answer: 2,
+	answerGif: "blackpanther.gif",
 }, {
 	question: "Captain America's shield is made of",
 	answerList: ["Vibranium", "Adamantium", "Kryptonite", "Chrome"],
-	answer: 0
+	answer: 0,
+	answerGif: "capshield.gif",
 }, {
 	question: "What is the name of the Marvel comic legend who has made an appearance in all the films of the Marvel Cinematic Universe?",
 	answerList: ["Jack Kirby", "Stan Lee", "Walter Norris", "Steve Rogers"],
-	answer: 1
+	answer: 1,
+	answerGiff: "stanlee.gif",
 }, {
 	question: "Thor’s hammer, Mjolnir, is made of metal from the heart of a dying what?",
 	answerList: ["Asteroid", "Comet", "Star", "Black Hole"],
-	answer: 2
+	answer: 2,
+	answerGif: "mjolnirstar.gif",
 }, {
 	question: "What is the name of the set of documents that regulate the activities of enhanced person?",
 	answerList: ["Sokovia Accords", "Wakanda Accords", "Stark Accords", "Paris Accords"],
-	answer: 0
+	answer: 0,
+	answerGif: "sokoviaaccords.gif",
 }, {
 	question: "Which Avenger does Loki briefly take the form of in Thor: The Dark World?",
 	answerList: ["Hulk", "Captain America", "Iron Man", "Hawkeye"],
-	answer: 1
+	answer: 1,
+	answerGif: "lokiascap.gif",
 }, {
 	question: "This daughter of Thanos is a member of the Guardians of the Galaxy.",
 	answerList: ["Mantis", "Gamora", "Jemma Simmons", "Scarlett Witch"],
-	answer: 1
+	answer: 1,
+	answerGif: "gamorathanos.gif",
 }, {
 	question: "How does Yondu control the Yaka arrow?",
 	answerList: ["By whistling", "By flicking his wrist", "With telepathy", "By winking"],
-	answer: 0
+	answer: 0,
+	answerGif: "yonduyaka.gif",
 }, {
 	question: "In the post-credit scene of Marvel’s The Avengers, what type of restaurant are the Avengers shown eating at?",
 	answerList: ["Shawarma", "Pizza", "Churrascaria", "Sushi"],
-	answer: 0
+	answer: 0,
+	answerGif: "avengersshawarma.gif",
 }, {
 	question: "Which of the following is Rocket not called in Guardians of the Galaxy Vol. 2?",
 	answerList: ["Smelly chinchilla", "Triangle-faced monkey", "Trash Panda", "Rat"],
-	answer: 0
+	answer: 0,
+	answerGif: "rocketracoon.gif",
 }, {
 	question: "Who is Thor's adopted sibiling?",
 	answerList: ["Odin", "Loki", "Erik Selvig", "Laufey"],
-	answer: 1
+	answer: 1,
+	answerGif: "loki.gif",
 }, {
 	question: "What is the name of Odin’s first child – the Goddess of Death?",
 	answerList: ["Helena", "Rona", "Lana", "Hela"],
-	answer: 3
+	answer: 3,
+	answerGif: "hela.gif",
 }, {
 	question: "Which of the Infinity Stones is embedded in Vision’s forehead?",
 	answerList: ["Life Stone", "Death Stone", "Earth Stone", "Mind Stone"],
-	answer: 3
+	answer: 3,
+	answerGif: "vision.gif",
 }, {
 	question: "What actor played Ant-Man",
 	answerList: ["Chris Hemsworth", "Paul Rudd", "Chris Pratt", "Benedict Cumberbatch"],
-	answer: 1
+	answer: 1,
+	answerGif: "paulruddantman.gif",
 }, {
 	question: "What is the name of the prison that Peter, Rocket, Gamora, and Groot were detained to?",
 	answerList: ["Killer", "Death Prison", "Death Wish", "Kyln"],
-	answer: 3
+	answer: 3,
+	answerGif: "kyln.gif",
 }, {
 	question: "In Thor, what is the alignment of the Nine Realms referred to as?",
 	answerList: ["The Alignment", "The Line", "The Rebirth", "The Convergence"],
-	answer: 3
+	answer: 3,
+	answerGif: "convergence.gif",
 }, {
 	question: "What is the name of Ego’s servant who has the ability to feel someone’s emotions by touching them?",
 	answerList: ["Marleen", "Juniper", "Fray", "Mantis"],
-	answer: 3
+	answer: 3,
+	answerGif: "mantis.gif",
 }, {
 	question: "Who is Tony Stark/Iron Man's best friend, who when dressed in his battle suit is known as War Machine?",
 	answerList: ["Nick Fury", "Bruce Banner", "James Rhodes", "Edwin Jarvis"],
-	answer: 2
+	answer: 2,
+	answerGif: "warmachinerhodes.gif",
 }, {
 	question: "In Iron Man, what is the name of Tony Stark’s personal butler?",
 	answerList: ["Jeeves", "Jarvis", "Alfred", "Jennings"],
-	answer: 1
+	answer: 1,
+	answerGif: "jarvis.gif",
 }, {
 	question: "What is the name of Tony Stark’s personal secretary?",
 	answerList: ["Pepper Potts", "Peggy Carter", "Phil Coulson", "Sam Wilson"],
-	answer: 0
+	answer: 0,
+	answerGif: "pepperpottstrash.gif",
 }, {
 	question: "Doctor Strange is played by which actor?",
 	answerList: ["Tom Hiddleston", "Benedict Cumberbatch", "Mark Ruffalo", "Chris Evans"],
-	answer: 1
+	answer: 1,
+	answerGif: "drstrange.gif",
 }, {
 	question: "What was Doctor Strange's profession before he became Sorcerer Supreme?",
 	answerList: ["Professor", "Dermatologist", "Dentist", "Neurosurgeon"],
-	answer: 3
+	answer: 3,
+	answerGif: "drstrangesurgeon.gif",
 }];
 
 // var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13','question14','question15'];
@@ -143,7 +167,7 @@ function newQuestion() {
 	// Clear main-display from previous game and set answered to true since it can be unanswered/correct/incorrect
 	$("#message-quote").empty();
 	$("#correct-answer").empty();
-	$("#answer-png").empty();
+	$("#answer-gif").empty();
 	answered = true;
 	// Randomly pick a questionNum from 1-25 from triviaQuestions array.
 	questionNum = Math.floor(Math.random() * triviaQuestions.length);
@@ -193,11 +217,10 @@ function answerPage() {
 	// Clear display first
 	$("#current-question").empty();
 	$(".newDivChoice").empty();
-	// Retrieve correct triviaQuestions.answer index and display answer
+	// Retrieve correct triviaQuestions.answer index and display answer and gif
 	var correctAnswerIndex = triviaQuestions[questionNum].answer;
 	var correctAnswerText = triviaQuestions[questionNum].answerList[correctAnswerIndex];
-
-	// $("#correct-answer").html("The correct answer is: " + correctAnswerText);
+	$("#answer-gif").html("<img src = 'assets/images/answers/"+ triviaQuestions[questionNum].answerGif +"' width = '400px'>");
 
 	// Increase correct/incorrect/or unanswered count and display message-quote as appropriate
 	if (answered === false) {
@@ -232,7 +255,7 @@ function scoreboard() {
 	$("#question-number").empty();
 	$("#message-quote").empty();
 	$("#correct-answer").empty();
-	$("#answer-png").empty();
+	$("#answer-gif").empty();
 	if (correctCounter < 3) {
 		$("#finished-quote").html(finishedQuote[0]);
 	}
