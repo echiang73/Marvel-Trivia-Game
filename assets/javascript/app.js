a621853962a0a155eaa35373ee0cf2937e35e3ad
Neo <echiang73@gmail.com>
2019-02-26 23:24:12 -0700

    adjust sizing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -274,7 +274,7 @@ function answerPage() {
 	// Retrieve correct triviaQuestions.answer index and display answer and gif
 	var correctAnswerIndex = triviaQuestions[questionNum].answer;
 	var correctAnswerText = triviaQuestions[questionNum].answerList[correctAnswerIndex];
-	$("#answer-gif").html("<img src = 'assets/images/answers/"+ triviaQuestions[questionNum].answerGif +"' width = '25%'>");
+	$("#answer-gif").html("<img src = 'assets/images/answers/"+ triviaQuestions[questionNum].answerGif +"' width='25%'>");
 
 	// Increase correct/incorrect/or unanswered count and display message-quote as appropriate
 	if (answered === false) {
